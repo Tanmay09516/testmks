d077e1ecfddac0da5e43c328bb8eb4b51b2e4134
Tanmay09516 <tanmay5tj@gmail.com>
2022-11-09 15:11:31 +0530

    delete 2

--- js/countdown-custom.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function() {
-        $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2022, 12, 20, 0)}); // year, month, date, hour
-        });
-});		
-
